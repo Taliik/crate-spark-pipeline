8ec778bbab0a413a8d19b5da3fbaa78348e289e8
Fabian <fabian.reisegger@students.fhv.at>
2017-08-21 16:24:58 +0200

    update

--- src/main/java/crate/ConnectionProvider.java
+++ src/main/java/crate/ConnectionProvider.java
@@ -1,49 +1,31 @@
 package crate;
 
+import org.apache.spark.ml.feature.NGram;
+import org.apache.spark.ml.feature.Tokenizer;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Locale;
 import java.util.Properties;
 
 public class ConnectionProvider {
     private static Properties properties;
-    private final String host;
-    private final int httpPort;
-    private final String user;
-
-    //private CloseableHttpClient httpClient = HttpClients.createSystem();
-    private Connection connection;
-    private SparkSession SpS;
+    private final String host = getProperty("crate.host");
+    private final int httpPort = Integer.parseInt(getProperty("crate.http.port"));
+    private final int psqlPort = Integer.parseInt(getProperty("crate.psql.port"));
+    private final String user = getProperty("crate.user");
+    private final String driver = "io.crate.client.jdbc.CrateDriver";
+    private SparkSession sparkSession;
 
     public ConnectionProvider() throws SQLException {
-        int psqlPort = Integer.parseInt(getProperty("crate.psql.port"));
-        httpPort = Integer.parseInt(getProperty("crate.http.port"));
-        host = getProperty("crate.host");
-        user = getProperty("crate.user");
 
-        String url = String.format(Locale.ENGLISH, "jdbc:crate://%s:%d/", host, psqlPort);
-        String driver = "io.crate.client.jdbc.CrateDriver";
-
-        SpS = SparkSession.builder()
+        sparkSession = SparkSession.builder()
                 .master("local[*]")
                 .appName("CrateSparkProject")
                 .getOrCreate();
-
-        Dataset<Row> dataset = SpS.read()
-                .format("jdbc")
-                .option("url", url)
-                .option("driver", driver)
-                .option("dbtable", "(select * from interns.raw_weather where date>'2013-01-01T03:59:00') as weather_data")
-                .option("user", user)
-                .load();
-
-        System.out.println(dataset.count());
-        dataset.show();
 
     }
 
@@ -64,8 +46,29 @@ public class ConnectionProvider {
     }
 
     public void getData() throws SQLException, IOException {
-    }
 
+        String url = String.format(Locale.ENGLISH, "jdbc:crate://%s:%d/", host, psqlPort);
+
+        Dataset<Row> dataset = sparkSession.read()
+                .format("jdbc")
+                .option("url", url)
+                .option("driver", driver)
+                .option("dbtable", "(select text from doc.test_twitter) as tweets")
+                .option("user", user)
+                .load();
+
+       // List<Row> dataList = dataset.collectAsList();
+
+        Tokenizer tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words");
+        Dataset<Row> wordsData = tokenizer.transform(dataset);
+
+        NGram ngramTransformer = new NGram().setN(3).setInputCol("words").setOutputCol("ngrams");
+
+        Dataset<Row> ngramDataFrame = ngramTransformer.transform(wordsData);
+        ngramDataFrame.select("ngrams").show(false);
+
+        sparkSession.stop();
+    }
 
 
 }
